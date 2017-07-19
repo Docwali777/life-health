9aee8d2dfcb0a253c39b2f26cdd5b1a01ed8fc7d
docwali777 <drgauvin@mgmcare.org>
2017-07-19 14:44:29 -0400

    first commit

---
+++ devServer.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack')
+const express = require('express');
+const app = express()
+const path = require('path');
+const PORT = process.env.PORT || 3000
+
+const webpackMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config.js')
+
+const compiler = webpack(config)
+
+const middleware = webpackMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: 'client',
+  // stats: {
+  //   colors: true,
+  //   progress: true,
+  //   chunkModules: false
+  // }
+})
+
+app.use(middleware)
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static('public'))
+app.use
+
+app.get('/*', (req, res)=>{
+  const filePath = path.resolve(path.join(__dirname, 'public/index.html'))
+  res.sendFile(filePath)
+})
+app.listen(PORT, ()=>{
+  console.log('server running');
+})
